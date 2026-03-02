2a9bc4636abd872e6f37f57ecfefb067ae599717
0fb8 <218385633+0fb8@users.noreply.github.com>
2026-03-02 16:31:23 +0900

    back: add server.js

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/api/time" && req.method === "GET") {
+    const currentTime = new Date().toISOString();
+    res.writeHead(200);
+    res.end(JSON.stringify({ time: currentTime }));
+  } else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: "Not Found" }));
+  }
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/`);
+});
