0320107ccd0b55788355bfb4b6bd9fd69fab9730
0fb8 <218385633+0fb8@users.noreply.github.com>
2026-03-02 18:32:21 +0900

    front: format time display using browser locale

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -12,7 +12,19 @@ button.addEventListener("click", async () => {
     const data = await response.json();
 
     console.log(data);
-    display.textContent = data.time;
+
+    const date = new Date(data.time);
+
+    const locale = navigator.language;
+    display.textContent = date.toLocaleString(locale, {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      weekday: "short",
+    });
   } catch (erorr) {
     console.error("Fetch error", error);
     display.textContent = "Error: Unable to connect server";
